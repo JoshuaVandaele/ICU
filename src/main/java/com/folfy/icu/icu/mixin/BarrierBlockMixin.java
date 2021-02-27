6d4b7d20a262c3c96d8bf1d6f0cc587c914ebace
Joshua Vandaële <joshua@vandaele.software>
2021-02-27 10:36:45 +0100

    Initial Push

---
+++ src/main/java/com/folfy/icu/icu/mixin/BarrierBlockMixin.java
@@ -0,0 +1,23 @@
+package com.folfy.icu.icu.mixin;
+
+import net.minecraft.block.BarrierBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.Direction;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BarrierBlock.class)
+public class BarrierBlockMixin extends Block {
+    public BarrierBlockMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Inject(at = @At("HEAD"), method = "getRenderType", cancellable = true)
+    public void getRenderType(BlockState state, CallbackInfoReturnable<BlockRenderType> info) {
+        info.setReturnValue(BlockRenderType.MODEL);
+    }
+}
